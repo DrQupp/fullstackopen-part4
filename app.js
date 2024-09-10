115106959db3ec5ce57fb2b21872c77044ef16a4
Qupp <qupp.sub@gmail.com>
2024-09-11 00:45:52 +0900

    Finish part 4b

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI)
@@ -13,4 +15,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 module.exports = app
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,28 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+  result = await blog.save()
+  response.status(201).json(result)
+})
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+blogsRouter.delete('/:id', async (request, response) => {
+  result = await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  console.log(body)
+  updatedBlog = await Blog.findByIdAndUpdate(request.params.id, body, {new:true})
+  console.log(updatedBlog)
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
